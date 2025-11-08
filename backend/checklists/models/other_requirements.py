0fa6ed6b99958b211933a1d20f44e872c40e373f
Suren Martirosyan <suren.v.mar@gmail.com>
2025-11-08 01:52:16 -0500

    create DepthChain model using mptt

--- backend/checklists/models/other_requirements.py
+++ backend/checklists/models/other_requirements.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 class Specialization(models.Model):
   name = models.CharField(max_length=255)
@@ -12,6 +13,22 @@ class NonCourseRequirement(models.Model):
     'Checklist',
     on_delete=models.CASCADE,
     related_name='non_course_requirements'
+  )
+
+class DepthChain(MPTTModel):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  course = models.ForeignKey(
+    'courses.Course',
+    on_delete=models.CASCADE,
+    related_name='+'
+  )
+  parent = TreeForeignKey(
+    'self',
+    blank=True,
+    null=True,
+    on_delete=models.CASCADE,
+    related_name='Children'
   )
 
 # Similar to course requisite logic
