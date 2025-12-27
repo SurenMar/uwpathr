3fa71edc4ae615e3bd3a0a4ffdd0340466f06b9f
Suren Martirosyan <suren.v.mar@gmail.com>
2025-12-27 16:38:36 -0500

    Allow missing entries for specialization's name and description fields.

--- backend/checklists/models/requirements.py
+++ backend/checklists/models/requirements.py
@@ -5,8 +5,8 @@ from mptt.models import MPTTModel, TreeForeignKey
 class Specialization(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-  name = models.CharField(max_length=255)
-  description = models.TextField()
+  name = models.CharField(max_length=255, null=True, blank=True)
+  description = models.TextField(null=True, blank=True)
 
   def __str__(self):
     return self.name
