eaf389b7d6a5f6f168a46103d127c04998702f62
Suren Martirosyan <suren.v.mar@gmail.com>
2026-01-14 05:19:05 -0500

    Add whitenoise to middleware and static storage.

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -153,6 +154,12 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STORAGES = {
+  'staticfiles': {
+    'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+  },
+}
 
 
 AUTHENTICATION_BACKENDS = {
