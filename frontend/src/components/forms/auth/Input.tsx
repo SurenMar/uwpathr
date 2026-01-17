3414c270cb527d9494c6cdb25df50bf55a63361d
Suren Martirosyan <suren.v.mar@gmail.com>
2026-01-17 12:54:37 -0500

    Add password autocomplete during registration.

--- frontend/src/components/forms/auth/Input.tsx
+++ frontend/src/components/forms/auth/Input.tsx
@@ -62,15 +62,16 @@ export default function Input({
 						))}
 					</select>
 				) : (
-					<input
-						id={labelId}
-						className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-						name={labelId}
-						type={type}
-						onChange={onChange}
-						value={value}
-						required={required}
-					/>
+								<input
+									id={labelId}
+									className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+									name={labelId}
+									type={type}
+									autocomplete={(labelId === 'password' || labelId === 're_password') ? 'new-password' : undefined}
+									onChange={onChange}
+									value={value}
+									required={required}
+								/>
 				)}
 			</div>
 		</div>
